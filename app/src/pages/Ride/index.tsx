9d69e2cd3e687a2d0d990decb0996da89f55cff4
asahi1199 <asahi.yoshida.1130@gmail.com>
2024-09-15 12:04:33 +0900

    [update] routing

---
+++ app/src/pages/Ride/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Map from "../../right_component/Map";
+import Drawer from "../../left_component/Drawer";
+
+const Ride = () => {
+  return (
+    <div>
+      <Map />
+      <Drawer />
+    </div>
+  );
+};
+
+export default Ride;
---
+++ app/src/pages/Search/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Map from "../../right_component/Map";
+import Drawer from "../../left_component/Drawer";
+
+const Search = () => {
+  return (
+    <div>
+      <Map />
+      <Drawer />
+    </div>
+  );
+};
+
+export default Search;
