9b0ac7c9d1baaa17ea7ef60bd880756eec8a5068
asahi1199 <asahi.yoshida.1130@gmail.com>
2024-09-15 18:36:23 +0900

    [add] recover Yuima's componetns

---
+++ app/src/left_component/components/CaptureButton/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css"; // ボタンのスタイルを定義するCSSファイル
+
+// コンポーネント定義
+const CaptureButton: React.FC = () => {
+  // ボタンがクリックされたときに呼び出される関数
+  const handleClick = (): void => {
+    window.location.href =
+      "https://qiita.com/Hashimoto-Noriaki/items/95d9fe027d169ce74218"; // リダイレクト先のURLを指定
+  };
+
+  return (
+    <button className="container" onClick={handleClick}>
+      この子をキャプチャする
+    </button>
+  );
+};
+
+export default CaptureButton;
