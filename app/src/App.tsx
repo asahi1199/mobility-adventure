bb2a1905a62866e186d61637445906ebe61c4dd3
asahi1199 <asahi.yoshida.1130@gmail.com>
2024-09-15 11:22:03 +0900

    [add] routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+// import { Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Map from "./right_component/Map";
+import Drawer from "./left_component/Drawer";
 
 import CroppableImage from './left_component/gesture_component';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test Croppable Image</h1>
-      <CroppableImage
-        src="big_map.png"
-        sensitivity={1}
-      />
-    </div>
+    <Router>
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">ホーム</Link>
+            </li>
+            <li>
+              <Link to="/map">Map</Link>
+            </li>
+            <li>
+              <Link to="/profile">プロフィール</Link>
+            </li>
+          </ul>
+        </nav> */}
+
+        {/* 各ページへのルート設定 */}
+        <Routes>
+          {/* <Route path="/" element={<Map />} /> */}
+          <Route path="/map" element={<Map />} />
+          <Route path="/drawer" element={<Drawer />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
